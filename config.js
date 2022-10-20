9cc65222b331fd5c1a038794f1af50ff68ec9668
khajinx11 <116270904+khajinx11@users.noreply.github.com>
2022-10-20 18:32:45 +0700

    Add files via upload
    created card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'https://www.youtube.com/watch?v=oYgwltU0hJE';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Mong bạn sẽ có môtj ngày 20/10 thật ý nghĩa bên người thân bạn bè';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Khaijinx.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
